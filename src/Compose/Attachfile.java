62201ff35db73a55ceb96bfd4b597e98227cc3bb
Vinod <vinod.obbineni@gmail.com>
2020-11-20 14:12:54 +0000

    First Code

---
+++ src/Compose/Attachfile.java
@@ -0,0 +1,14 @@
+package Compose;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Attachfile {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://www.google.com/intl/en-GB/gmail/about/#");
+	}
+
+}
